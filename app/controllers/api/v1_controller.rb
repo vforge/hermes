8b91bcb2c0221dce2a34e8d8fab3a35fbb88e88c
V <bartosz.bentkowski@gmail.com>
2014-11-30 19:39:19 +0100

    Test mode for API v 1

--- app/controllers/api/v1_controller.rb
+++ app/controllers/api/v1_controller.rb
@@ -1,12 +1,12 @@
 class API::V1Controller < ActionController::Base
   def get
     @object = Ad.search(params).first
-    @object.increment!(:count_requests)
+    @object.increment!(:count_requests) unless params[:test]
   end
 
   def click
     @object = Ad.with_status(:active).find(params[:id])
-    @object.increment!(:count_clicks)
+    @object.increment!(:count_clicks) unless params[:test]
 
     redirect_to @object.href
   end
