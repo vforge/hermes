edacb8d32d02747f4952c714ac8f9b17e97394e6
V <bartosz.bentkowski@gmail.com>
2014-11-30 22:05:13 +0100

    Update seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
-User.new(email:'admin', password: 'admin', password_confirmation: 'admin', role: :admin).quiet_save
-AdCampaign.create!(name: 'Lorem', company: 'Ipsum', starting_at: 10.hours.ago, ending_at: 2.years.from_now)
+User.new(id: -1, email: 'system', password: 'system', password_confirmation: 'system', role: :admin).quiet_save
+
+if Rails.env.development?
+  AdCampaign.create!(name: 'Lorem', company: 'Ipsum', starting_at: 10.hours.ago, ending_at: 2.years.from_now)
+end
