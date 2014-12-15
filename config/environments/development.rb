6291dc386432892a57e842d97ec0f12a9b415596
V <bartosz.bentkowski@gmail.com>
2014-12-15 17:49:21 +0100

    Configure cache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,7 @@ Mercury::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.cache_store = :memory_cache
 end
 STDOUT.sync = true
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,6 @@ Mercury::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.cache_store = :null_store
 end
