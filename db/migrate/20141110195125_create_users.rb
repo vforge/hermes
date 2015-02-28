ca204da6dc38cc355c97cc29270329cd91c14f9a
V <bartosz.bentkowski@gmail.com>
2015-02-28 14:39:30 +0100

    Make binary password hash

--- db/migrate/20141110195125_create_users.rb
+++ db/migrate/20141110195125_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string   :email
-      t.string   :password_digest
+      t.binary   :password_digest
       t.string   :role
       t.string   :locale
       t.datetime :deleted_at
